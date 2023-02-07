da6733d50d42a99442933fcd6d581792d2dcd8f9
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-07 12:14:36 +0300

    project complete

---
+++ index.js
@@ -0,0 +1,16 @@
+let randomNumber1 = Math.floor((Math.random() * 6) + 1);
+let randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+let image1 = "images/dice" + randomNumber1 + ".png";
+let image2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".dice .img1").setAttribute("src", image1);
+document.querySelector(".dice .img2").setAttribute("src", image2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "🚩 it's a TIE 🚩";
+}
